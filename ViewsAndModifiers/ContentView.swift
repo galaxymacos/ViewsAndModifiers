ce650d2b3d3e9004d35a7afc773e918466511ba2
Xun Ruan <galaxymacos@gmail.com>
2021-07-05 07:46:01 +0800

    Why does swift use "some View" for its view type

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,38 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-
-        VStack(spacing: 50){
-            Button("Hello world"){
-                // ModifiedContent<ModifiedContent<Button<Text>, _BackgroundModifier<Color>>, _FrameLayout>
-                print(type(of:self.body))
-                
-            }
-            .background(Color.red)
-            .frame(width: 200, height: 200)
-            
-            Button("Hello world"){
-                // ModifiedContent<ModifiedContent<Button<Text>, _BackgroundModifier<Color>>, _FrameLayout>
-                print(type(of:self.body))
-                
-            }
-            .frame(width: 200, height: 200)
-            .background(Color.red)
-            
-            
-            Button("rainbow border"){
-                
-            }
-            .padding()
-            .border(Color.blue)
-            .padding()
-            .border(Color.yellow)
-            .padding()
-            .border(Color.green)
+    // not allowed
+//    var body: View{
+//        Text("efe")
+//    }
+    
+    // Swift wants to know the specific type of body
+//    var body: Text{
+//        Text("lalala")
+//    }
+    
+    // The return type is so complicated, so we return some View
+    var body: VStack<TupleView<(Text, Text)>>{
+        //
+        let stack = VStack{
+            Text("1")
+            Text("2")
         }
-        
+        print(type(of: stack))
+        return stack
     }
+    
+    
+//    var body: some View {
+//
+//    }
+    // We don't know what type of this Button, so we return some View
+//    var body: some View{
+//        Button("Hello World") {
+//            print(type(of: self.body))
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(Color.red)
+//    }
+    
+    //
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
