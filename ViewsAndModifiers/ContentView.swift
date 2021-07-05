3ca7613773d78e5ba49c2254564dbccb0a3cbe5a
Xun Ruan <galaxymacos@gmail.com>
2021-07-05 08:25:14 +0800

    Environment modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -11,10 +11,20 @@ import SwiftUI
 struct ContentView: View {
     @State var isRedColor = false
     var body: some View{
-        // make a color-changing button by clicking
-        Button("I am a color-clicking-changing button"){
-            isRedColor.toggle()
-        }.foregroundColor(isRedColor ? Color.red: Color.blue)
+        
+        // blur is a regular modifier
+//        VStack{
+//            Text("1").blur(radius: 1, opaque: false)
+//            Text("2")
+//        }.blur(radius: 1, opaque: false)
+        
+        // Environment modifier will override by child
+        VStack {
+            Text("Gryffindor").font(.body)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+        }.font(.largeTitle)
     }
     
 }
